ad9e947a673d122ea0758febf9ab4c6cd950bcd9
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-25 22:22:49 +0530

    -

---
+++ Codeforces/CodeforcesBetaRound77(Div2Only)/AFootball/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+    int count = 0;
+
+    for (int i =0; i<s.length(); i++){
+        if (s[i] == s[i+1]){
+            count++;
+        }
+        else count = 0;
+
+        if (count == 6){
+            cout<<"YES"<<endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+
+    return 0;
+}
