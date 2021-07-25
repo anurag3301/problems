377138211b150b6d5d27504151cf9528a8bc11cf
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-25 22:16:06 +0530

    -

---
+++ Codeforces/CodeforcesBetaRound85(Div2Only)/APetyaandStrings/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+    if(s1.compare(s2) == 0)cout << 0 << endl;
+    else if(lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end())) cout << -1 << endl;
+    else cout << 1 << endl;
+}
