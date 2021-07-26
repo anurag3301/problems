b78d650656b0b0771970dbeb8aea24c66fb818b1
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-26 11:39:58 +0530

    Word

---
+++ Codeforces/CodeforcesBetaRound55(Div2)/AWord/solution.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/problemset/problem/59/A
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cctype>
+using namespace std;
+
+int main(){
+	string word;
+	cin >> word;
+	int u=0, l=0;
+	for(char x : word){
+		if(isupper(x))u++;
+		else l++;
+	}
+	if(u>l)transform(word.begin(), word.end(), word.begin(), ::toupper);
+	else transform(word.begin(), word.end(), word.begin(), ::tolower);
+	cout << word << endl;
+}
