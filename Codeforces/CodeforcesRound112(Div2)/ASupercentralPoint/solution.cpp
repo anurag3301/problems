800fa6f40e8b19ac55160fc96f2d3bc69a7a5038
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-09-25 20:26:59 +0530

    165A - Supercentral Point

--- Codeforces/CodeforcesRound112(Div2)/ASupercentralPoint/solution.cpp
+++ Codeforces/CodeforcesRound112(Div2)/ASupercentralPoint/solution.cpp
@@ -1,12 +1,7 @@
 // https://codeforces.com/problemset/problem/165/A
 #include<bits/stdc++.h>
-#include<iostream>
-#include<algorithm>
-#include <utility>
 using namespace std;
 
-/*
-*/
 int main(){
   int x, y, n; cin >>n;
   vector<pair<int, int>> p;
@@ -17,15 +12,14 @@ int main(){
   int count = 0;
   int u=0, b=0, r=0, l=0;
   for(auto i:p){
+    u=0; b=0; r=0; l=0;
     for(auto j:p){
       if(i.first == j.first && i.second > j.second)b = 1;
       if(i.first == j.first && i.second < j.second)u = 1;
       if(i.first > j.first && i.second == j.second)l = 1;
       if(i.first < j.first && i.second == j.second)r = 1;
       if(u && b && r && l){
-        cout << i.first << " " << i.second << endl;
         count++;
-        u=0; b=0; r=0; l=0;
         break;
       }
     }
