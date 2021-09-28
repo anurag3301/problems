2089fc01aa5f38e120266f8cae7e01b023bb7956
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-09-28 14:05:15 +0530

    219A - k-String

--- Codeforces/CodeforcesRound135(Div2)/AkString/solution.cpp
+++ Codeforces/CodeforcesRound135(Div2)/AkString/solution.cpp
@@ -3,5 +3,27 @@
 using namespace std;
 
 int main(){
-
+  int k, x; cin >> k;
+  string s;
+  cin >> s;
+  map<char, int> m;
+  for(char i:s){
+    m[i]++;
+  }
+  for(auto &i:m){
+    if(i.second % k == 0){
+      i.second /= k;
+    }
+    else{
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+  string f = "";
+  while(k--){
+    for(auto i:m){
+      for(int j=0; j<i.second; j++)f+=i.first;
+    }
+  }
+  cout << f << endl;
 }
