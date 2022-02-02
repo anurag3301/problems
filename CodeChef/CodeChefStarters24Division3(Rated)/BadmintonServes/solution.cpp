2c56dff960629df73094b925e322bf26498efc17
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-02-02 20:14:00 +0530

    Badminton Serves

---
+++ CodeChef/CodeChefStarters24Division3(Rated)/BadmintonServes/solution.cpp
@@ -0,0 +1,10 @@
+// https://www.codechef.com/START24C/problems/BADMINTON
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t, p; cin >> t;
+    while(t--){
+        cin >> p;
+        cout <<(p/2)+1<<endl;
+    }
+}
