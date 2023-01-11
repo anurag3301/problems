664e11ae9359b863491040e3e76abb74709b294d
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2023-01-11 22:40:35 +0530

    B. Petya and Square

---
+++ Codeforces/CodeforcesBetaRound85(Div2Only)/BPetyaandSquare/solution.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/contest/112/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, x, y; cin >> n >> x >> y;
+
+    if(x!=n/2 || y!=n/2){
+        cout << "YES" << endl;
+        return 0;
+    }
+}
