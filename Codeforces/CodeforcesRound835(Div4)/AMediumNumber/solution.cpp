fbf20c541472f3abe950f523fdf140969eceefc8
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2023-01-11 22:41:53 +0530

    A. Medium Number

---
+++ Codeforces/CodeforcesRound835(Div4)/AMediumNumber/solution.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/contest/1760/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, a, b, c, mx, mi; cin >> n;
+    while(n--){
+        cin >> a >> b >> c;
+        mx = max({a, b, c});
+        mi = max({a, b, c});
+    }
+}
