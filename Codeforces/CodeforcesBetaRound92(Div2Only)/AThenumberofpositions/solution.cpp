f006f6d0f9eb389d63c3f723ac09906410706294
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-01-26 12:57:23 +0530

    o(1) 124A - The number of positions

--- Codeforces/CodeforcesBetaRound92(Div2Only)/AThenumberofpositions/solution.cpp
+++ Codeforces/CodeforcesBetaRound92(Div2Only)/AThenumberofpositions/solution.cpp
@@ -3,16 +3,6 @@
 using namespace std;
 
 int main(){
-  int a, n, b, x; cin >> n >> a >> b;
-  set<int> front;
-  set<int> back;
-  for(int i=a+1; i<=n; i++){
-    front.insert(i);
-  }
-  for(int i=n; i>=n-b; i--){
-    back.insert(i);
-  }
-  set<int> inter;
-  set_intersection(front.begin(), front.end(), back.begin(), back.end(), inserter(inter, inter.begin()));
-  cout << inter.size() << endl;
+    int a, n, b, x; cin >> n >> a >> b;
+    cout << min(n-a, b+1) << endl;
 }
